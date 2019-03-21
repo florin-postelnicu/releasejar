0743181f96ddfb9b76c1af52676b6aaf6bacb778
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-03-21 14:33:01 -0700

    Add files via upload

---
+++ GuessGamepj/src/com/example/guessgame/GameLauncher.java
@@ -0,0 +1,8 @@
+package com.example.guessgame;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
---
+++ GuessGamepj/src/com/example/guessgame/Player.java
@@ -0,0 +1,11 @@
+package com.example.guessgame;
+
+
+public class Player {
+    int number = 0;
+
+    public void guess() {
+        number = (int) (Math.random() * 10);
+        System.out.println("I'am guessing " + number);
+    }
+}
