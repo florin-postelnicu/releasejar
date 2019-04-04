810fd2ee4896b2eb60692d06781248784f29405a
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-04-04 14:52:50 -0700

    Add files via upload

--- WindowAppsDialog/JavaJOptionPaneShowInputDialog.java
+++ WindowAppsDialog/JavaJOptionPaneShowInputDialog.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 A variable assigned through name is reported to
 the whole program by defining it as static and public variable,
 and reassigning it inside the constructor
+The static keyword denotes that a member variable, or method,
+can be accessed without requiring an instantiation of the class to which it belongs.
+In simple terms, it means that you can call a method, even if you've never created the object to which it belongs!
  */
 
 public class JavaJOptionPaneShowInputDialog {
@@ -26,7 +29,7 @@ public class JavaJOptionPaneShowInputDialog {
 
     public static void main(String[] args) {
         new JavaJOptionPaneShowInputDialog();
-        System.out.println(JavaJOptionPaneShowInputDialog.name1);
+        System.out.println(name1);
 
 
     }
