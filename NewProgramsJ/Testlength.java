1e729ff9b1baa4c86bf747a31b5fac61c8e759cb
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-04-18 10:06:37 -0700

    Add files via upload

--- NewProgramsJ/Testlength.java
+++ NewProgramsJ/Testlength.java
@@ -9,10 +9,33 @@ public class Testlength {
         St = console.nextLine();
     }
 
+
     public static void main(String[] args) {
+        System.out.println("Enter your String:");
+        char[] car;
+
+
         Testlength Tl = new Testlength();
         Tl.setSt();
-        System.out.println("The String is " + Tl.getSt() + " \nand its length is " + Tl.getSt().length());
+        car = new char[Tl.getSt().length()];
+        System.out.println("The String is: " + Tl.getSt() + " \nand its length is " + Tl.getSt().length());
+
+        for(int i=0; i< Tl.getSt().length(); i++){
+            car[i] = Tl.getSt().charAt(i);
+        }
+        char[] stringToCharArray = Tl.getSt().toCharArray();
+        String bezzo ;
+        bezzo = "";
+
+        for (char output : stringToCharArray){
+            if (output == 'e')
+            output = 'M';
+            bezzo+= (output);
+        }
+
+        System.out.println(stringToCharArray);
+        System.out.println(bezzo);
+
     }
 
 }
