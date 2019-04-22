80e5108e9ea9a7e4e9c39042b61b9c7d17e70a2c
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-04-22 11:07:03 -0700

    Add files via upload

--- NewProgramsJ/JasonSimpleReadExample.java
+++ NewProgramsJ/JasonSimpleReadExample.java
@@ -10,6 +10,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
 
+/*
+I got an error for using Iterator<String> with an Array JSONArray,
+
+Exception in thread "main" java.lang.ClassCastException: class org.json.simple.JSONObject cannot be cast
+to class java.lang.String (org.json.simple.JSONObject is in unnamed module of loader 'app'; java.lang.String is
+in module java.base of loader 'bootstrap')
+at programforstudents.JasonSimpleReadExample.main(JasonSimpleReadExample.java:39)
+
+The problem is solvable by eliminating the <String> identifier from Iterator.
+So, as long as we deal with a regular Array, all the methods valid in java.lang can be applied.
+ */
+
 public class JasonSimpleReadExample {
     public static void main(String[] args) {
         JSONParser parser = new JSONParser();
@@ -24,12 +36,15 @@ public class JasonSimpleReadExample {
             String name = (String) jsonObject.get("name");
             System.out.println(name);
 
-            long age = (Long) jsonObject.get("age");
+            long age = (Long) jsonObject.get("Age");
             System.out.println(age);
 
             // loop array
-            JSONArray msg = (JSONArray ) jsonObject.get("messages");
-            Iterator<String> iterator = msg.iterator();
+
+            JSONArray ctcp = (JSONArray ) jsonObject.get("ContactPhones");
+//            Iterator<String> iterator = ctcp.iterator(); // Se the comment, and replace it with :
+
+            Iterator iterator = ctcp.iterator();
             while(iterator.hasNext()){
                 System.out.println(iterator.next());
             }
