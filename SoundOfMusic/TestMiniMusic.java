dbbaea3fe1d4b2327d3c6209ee98d454ae50751d
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2019-05-07 15:09:52 -0700

    Add files via upload

---
+++ SoundOfMusic/TestMiniMusic.java
@@ -0,0 +1,23 @@
+package mypackage01;
+
+public class TestMiniMusic {
+    public static void main(String[] args) {
+        MiniMusicCmdLine  minim = new MiniMusicCmdLine();
+
+        minim.play(102, 30);
+        minim.play(80, 20);
+        minim.play(40, 67);
+        minim.play(102, 30);
+        minim.play(80, 20);
+        minim.play(40, 67);
+        minim.play(102, 30);
+        minim.play(80, 20);
+        minim.play(40, 67);
+        minim.play(102, 30);
+        minim.play(80, 20);
+        minim.play(40, 67);
+    }
+    }
+
+
+
